f5b489d8c144de36dce079d9b124163075001179
9dev <9devmail@gmail.com>
2015-01-05 10:37:20 +0100

    Updated demo models

--- demo/main/models.py
+++ demo/main/models.py
@@ -5,6 +5,9 @@ class Text(models.Model):
     text = models.CharField(max_length=1000)
     title = models.CharField(max_length=100)
     user = models.ForeignKey('auth.User', blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('main:text_detail', kwargs={'pk':str(self.id)})
     
 class Image(models.Model):
     image = models.ImageField(upload_to='images')
