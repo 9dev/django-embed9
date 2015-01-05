6ef3ef8bf1f03e25812e59dc4a952e9e67a3f36d
9dev <9devmail@gmail.com>
2015-01-05 10:33:12 +0100

    Updated demo urls

--- demo/demo/urls.py
+++ demo/demo/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
 	url(r'^admin/', include(admin.site.urls)),
+	url(r'^embed/', include('embed9.urls', namespace='embed9')),
 	url(r'^', include('main.urls', namespace='main')),
 )
+
+if settings.DEBUG:
+	urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
