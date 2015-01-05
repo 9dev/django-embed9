fa1bc8820c4e293d2f7a1765988d9316091ec74e
9dev <9devmail@gmail.com>
2015-01-05 10:31:51 +0100

    Added index view in demo

--- demo/main/views.py
+++ demo/main/views.py
@@ -1,6 +1,10 @@
 from django.views.generic import DetailView, ListView
+from django.http import HttpResponse
 
 from main.models import Image, Text
+
+def index(request):
+	return HttpResponse('Go to /texts or /images')
 
 class ImageDetailView(DetailView):
 	model = Image
