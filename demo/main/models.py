07361ed628029b56d4889f68d6bbd32a4295e9da
9dev <9devmail@gmail.com>
2015-01-05 10:24:15 +0100

    Added demo models

--- demo/main/models.py
+++ demo/main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+class Text(models.Model):
+    text = models.CharField(max_length=1000)
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey('auth.User', blank=True, null=True)
+    
+class Image(models.Model):
+    image = models.ImageField(upload_to='images')
+    title = models.CharField(max_length=100)
+    user = models.ForeignKey('auth.User', blank=True, null=True)
+    awesome = models.BooleanField()
+
+    def get_absolute_url(self):
+        return reverse('main:image_detail', kwargs={'pk':str(self.id)})
