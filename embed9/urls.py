9ad39f8b08e27e3975e99cd748c34655cf1729bb
9dev <9devmail@gmail.com>
2015-01-06 14:04:32 +0100

    Added loader url

--- embed9/urls.py
+++ embed9/urls.py
@@ -4,4 +4,5 @@ from embed9 import views
 
 urlpatterns = patterns('',
     url(r'^widget/(?P<app>[0-9a-z_.]+)/(?P<model>[0-9a-z_]+)/(?P<pk>\d+)$', views.widget, name='widget'),
+    url(r'^loader/(?P<app>[0-9a-z_.]+)/(?P<model>[0-9a-z_]+)/(?P<pk>\d+)/(?P<widget_id>\d+)$', views.loader, name='loader'),
 )
