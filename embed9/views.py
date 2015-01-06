4c174ba3b17f14c2103c471f56bdb0f0ccdd52cf
9dev <9devmail@gmail.com>
2015-01-06 14:04:10 +0100

    Added loader view

--- embed9/views.py
+++ embed9/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.core.urlresolvers import reverse
 from django.views.decorators.clickjacking import xframe_options_exempt
 
 from embed9.utils import get_appmodel, get_embeddable
@@ -8,9 +9,22 @@ from embed9.utils import get_appmodel, get_embeddable
 def widget(request, app, model, pk):
     model_ = get_appmodel(app, model)
     embed = get_embeddable(app, model_.__name__)
-    obj = get_object_or_404(model_, pk=pk)    
+    obj = get_object_or_404(model_, pk=pk) 
     template = embed.get_widget_template()
     
     return render_to_response(template, {
         model : obj,
+    }, RequestContext(request))
+
+@xframe_options_exempt
+def loader(request, app, model, pk, widget_id):
+    model_ = get_appmodel(app, model)
+    embed = get_embeddable(app, model_.__name__)
+    obj = get_object_or_404(model_, pk=pk)
+    template = embed.get_loader_template()
+    
+    return render_to_response(template, {
+        model : obj,
+        'widget_id' : widget_id,
+        'iframe_url' : reverse('embed9:widget', kwargs={'app': app, 'model': model, 'pk': pk}),
     }, RequestContext(request))
