a436cf8443ecd66b9af2d34603d0aa6c0c44e8b9
9dev <9devmail@gmail.com>
2015-01-06 14:00:25 +0100

    Added embed code settings to Embeddable

--- embed9/main.py
+++ embed9/main.py
@@ -6,3 +6,13 @@ class Embeddable:
         return self.widget_template
     widget_template = 'embed9/widget.html'
     
+    def get_code_template(self):
+        """ Return name of the template containing embed code for external websites """
+        return self.code_template
+    code_template = 'embed9/code.html'
+    
+    def get_loader_template(self):
+        """ Return name of the template containing javascript loader of the widget """
+        return self.loader_template
+    loader_template = 'embed9/loader.js'
+    
