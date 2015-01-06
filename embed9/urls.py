74e9037da0c5d3b05323da2044c3b257da50aad0
9dev <9devmail@gmail.com>
2015-01-06 16:07:24 +0100

    Added url for widget preview

--- embed9/urls.py
+++ embed9/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
+from django.views.decorators.csrf import csrf_exempt
 
 from embed9 import views
 
 urlpatterns = patterns('',
     url(r'^widget/(?P<app>[0-9a-z_.]+)/(?P<model>[0-9a-z_]+)/(?P<pk>\d+)$', views.widget, name='widget'),
     url(r'^loader/(?P<app>[0-9a-z_.]+)/(?P<model>[0-9a-z_]+)/(?P<pk>\d+)/(?P<widget_id>\d+)$', views.loader, name='loader'),
+    url(r'^preview/(?P<app>[0-9a-z_]+)/(?P<model>[0-9a-z_]+)/(?P<pk>\d+)$', csrf_exempt(views.PreviewView.as_view()), name='preview'),
 )
