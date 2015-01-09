03565bbb204ede62bf6f7d375bd8458b0f309ad2
9dev <9devmail@gmail.com>
2015-01-09 21:02:10 +0100

    Updated demo model

--- demo/main/models.py
+++ demo/main/models.py
@@ -13,7 +13,7 @@ class Image(models.Model):
     image = models.ImageField(upload_to='images')
     title = models.CharField(max_length=100)
     user = models.ForeignKey('auth.User', blank=True, null=True)
-    awesome = models.BooleanField()
+    awesome = models.BooleanField(default=False)
 
     def get_absolute_url(self):
         return reverse('main:image_detail', kwargs={'pk':str(self.id)})
