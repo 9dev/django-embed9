4c2a6d8c72e25bdf809191107e9f8a5145c4d85d
9dev <9devmail@gmail.com>
2015-01-09 21:28:59 +0100

    Updated demo embed

--- demo/main/embed.py
+++ demo/main/embed.py
@@ -1,7 +1,10 @@
 from embed9.main import Embeddable
 
+from main.forms import ImageWidgetForm
+
 class ImageEmbed(Embeddable):
     widget_template = 'main/image_widget.html'
+    form_class = ImageWidgetForm
 
 class TextEmbed(Embeddable):
     pass
