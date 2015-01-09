671bc62d3050575b69751cd90e55fd14219d0b2b
9dev <9devmail@gmail.com>
2015-01-09 21:04:25 +0100

    Added form to demo

--- demo/main/forms.py
+++ demo/main/forms.py
@@ -1,4 +1,12 @@
-from django.forms import Form
+from django import forms
 
-class MyForm(Form):
-	pass
+from main.models import Image
+
+class ImageWidgetForm(forms.ModelForm):
+	width = forms.IntegerField(max_value=500, min_value=75, initial=200, help_text='Width of the widget in pixels')
+	color = forms.RegexField('^[A-Fa-f0-9]{6}$', initial='543674', help_text='Background color for the widget in HEX (without the "#")')
+
+	class Meta:
+		model = Image
+		fields = ['awesome',]
+		
