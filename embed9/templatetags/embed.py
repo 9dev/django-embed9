04d47592f286ac34232f3e41150e672455cb477f
9dev <9devmail@gmail.com>
2015-01-09 21:28:39 +0100

    Updated templatetags

--- embed9/templatetags/embed.py
+++ embed9/templatetags/embed.py
@@ -2,14 +2,15 @@ from django.template import Library, loader, Context
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.utils.html import escape
+from django.utils.safestring import mark_safe
 
 from random import randrange
 
-from embed9.utils import get_embeddable
+from embed9.utils import get_embeddable, get_encoded_params, get_form_initial
 
 register = Library()
 
-def widget(obj):
+def widget(obj, *args, **kwargs):
     """ Render embed code template """
     model_name = obj.__class__.__name__
     model_name_lower = model_name.lower()
@@ -19,18 +20,28 @@ def widget(obj):
     embed = get_embeddable(app_name, model_name)
     template_name = embed.get_code_template()
     t = loader.get_template(template_name)
+    form_class = embed.get_form_class()
+    params = get_form_initial(form_class)
+    kwparams = kwargs.pop('params', None)
+    
+    for n,v in kwargs.items():
+        params[n] = v
+        
+    for n,v in kwparams.items():
+        params[n] = v
     
     return t.render(Context({
         model_name_lower : obj,
         'widget_id' : widget_id,
         'domain' : Site.objects.get_current().domain,
-        'loader_url' : reverse('embed9:loader', kwargs={'app': app_name, 'model': model_name_lower, 'pk': obj.pk, 'widget_id': widget_id}),
+        'loader_url' : mark_safe(reverse('embed9:loader', kwargs={'app': app_name, 'model': model_name_lower, 'pk': obj.pk, 'widget_id': widget_id}) + get_encoded_params(params)),
+        'params' : params,
     }))
 
 @register.simple_tag
-def widget_preview(obj):
-    return widget(obj)
+def widget_preview(obj, *args, **kwargs):    
+    return widget(obj, *args, **kwargs)
 
 @register.simple_tag
-def widget_code(obj):
-    return escape(widget(obj))
+def widget_code(obj, *args, **kwargs):
+    return escape(widget(obj, *args, **kwargs))
